1556336e72d3a6a53c80e455c84489d61744b4cc
Stefano Miceli <mic.ninja@gmail.com>
2023-12-06 11:07:46 +0100

    first commit

---
+++ callbacks.js
@@ -0,0 +1,32 @@
+const waitAndMessage = () => {
+  setTimeout(() => {
+    console.log("sono passati 3.5s");
+  }, 3500);
+
+  console.log("Io sono l'ultimo");
+};
+
+// -------------------------------------------------------------------------------------------
+
+const phoneCall = (id, callback) => {
+  const randTime = Math.floor(Math.random() * 3000);
+
+  setTimeout(() => {
+    callback(id, randTime); // normalAnswer(randTime); grannysAnswer(randTime);  ((time) => console.log("📞 Che vuoi?!? " + time / 1000 + "s"))()
+  }, randTime);
+
+  console.log("☎️ *Il telefono sta squillando* (" + id + ")");
+};
+
+const normalAnswer = (id, time) => {
+  console.log(id + ") 📞 Pronto? Chi parla? " + time / 1000 + "s");
+};
+
+const grannysAnswer = (id, time) => {
+  console.log(id + ") 📞 Pronto? Tesoro hai mangiato?? " + time / 1000 + "s");
+};
+
+phoneCall(1, normalAnswer);
+phoneCall(2, grannysAnswer);
+phoneCall(3, (id, time) => console.log(id + ") 📞 Che vuoi?!? " + time / 1000 + "s"));
+// phoneCall();
